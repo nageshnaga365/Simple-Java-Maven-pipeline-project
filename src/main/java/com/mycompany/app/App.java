3c46e975496318ed102ae9f31ccedeb4c8af6221
nageshnaga365 <87369452+nageshnaga365@users.noreply.github.com>
2026-07-11 12:05:22 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,7 +4,7 @@ public class App {
     public App(){
     }
     public String getMessage(){
-        return "Hello World";
+        return "Hello World!";
     }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
